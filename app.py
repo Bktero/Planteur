0137ef6e1117ff0604a6498e7d8c41f65b298c7f
Pierre Gradot <plgbasura@gmail.com>
2016-05-03 10:05:50 +0200

    Provide a more complete example with a plant description file

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
-"""
-This is the entry point for the Planteur application.
-"""
+""" This is the entry point for the Planteur application."""
 import json
 import logging
 
+import plant
+
 __author__ = 'pgradot'
 
-# Application startup
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
-                    level=logging.INFO)
 
-# Main work
-logging.info('Planteur application starts')
+class App(object):
+    def __init__(self, config_pathname, plant_pathname):
+        """ Create a new application.
 
-logging.info('Loading configuration...')
-with open('config.json') as file:
-    dict = json.load(file)
-    parameter = dict['category']['param']
-    print(dict)
-    print(parameter)
-    parameter = dict['another_category']['other_param']
-    print(parameter)
+        :param config_pathname: the pathname to the configuration file
+        :type config_pathname: str
+        :param plant_pathname: the pathname to the plant description file
+        :type plant_pathname: str
+        """
+        self.config_pathname = config_pathname
+        self.plant_pathname = plant_pathname
 
-logging.warning('Planteur application ends')
+        # Default configuration for logging
+        logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG)
+
+    def run(self):
+        logging.info('Planteur application starts')
+
+        # Load configuration from configuration file
+        self._load_configuration()
+
+        # Load plants from description file
+        self._load_plants()
+
+    def _load_configuration(self):
+        logging.info('Loading configuration...')
+        with open(self.config_pathname) as file:
+            json_dict = json.load(file)
+            parameter = json_dict['category']['param']
+            print(json_dict)
+            print(parameter)
+            parameter = json_dict['another_category']['other_param']
+            print(parameter)
+
+    def _load_plants(self):
+        loader = plant.PlantLoader('plants.json')
+        print(loader)
+        plants = loader.load()
+        print(plants)
+
+if __name__ == '__main__':
+    app = App('config.json', 'plants.json')
+    app.run()
---
+++ plant.py
@@ -0,0 +1,60 @@
+""" This module provides classes to manipulate plants in the python world.
+"""
+
+import json
+import logging
+
+__author__ = 'pgradot'
+
+
+class Plant(object):
+    """A Plant is python object that represent a plant in the real world."""
+    def __init__(self, uid, name):
+        """Create a new plant.
+
+        :param uid: a unique identifier of this plant
+        :type uid: int
+        :param name: the human-readable name of this plant
+        :type name: str
+        """
+        self.uid = uid
+        self.name = name
+
+    def __str__(self):
+        return '{}: uid={}, name={}'.format(self.__class__.__name__, self.uid, self.name)
+
+
+class PlantLoader(object):
+    """A PlantLoader loads a JSON description file and creates the corresponding
+    set of plants.
+    """
+    def __init__(self, pathname):
+        """Create a new loader.
+
+        :param pathname: the pathname to the file that contains the descriptions of the plants
+        :type pathname: str
+        """
+        self.pathname = pathname
+
+    def __str__(self):
+        return self.__class__.__name__ + ": " + self.pathname
+
+    def load(self):
+        """Load the plants.
+
+        Open the JSON description file represented by the pathname.
+        Create a python object for each described plant.
+
+        :return: the list of these objects
+        """
+        plants = []
+        with open(self.pathname) as file:
+            json_dict = json.load(file)
+            for index, plant_dict in enumerate(json_dict['plants']):
+                uid = index
+                name = plant_dict["name"]
+                plant = Plant(uid, name)
+                logging.debug('Loading plant: %s', plant)
+                plants.append(plant)
+
+        return plants
